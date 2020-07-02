d6f84fe3176e71d515f508a36639797a56238f75
angadpreetmalhi <57296198+angadpreetmalhi@users.noreply.github.com>
2020-07-01 21:47:31 -0700

    Update settings.py

--- HTML_list_maker/settings.py
+++ HTML_list_maker/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'd$u_kolmu&^^qod+!%t8dcl2clfs192azdpu3hk@op+$jqx)0x'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','d$u_kolmu&^^qod+!%t8dcl2clfs192azdpu3hk@op+$jqx)0x')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
